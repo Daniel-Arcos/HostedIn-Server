8069be922e5eb1bf871dd7b98b7952fa15ba8dc3
Eduardo García Díaz <eduardo.gd17@gmail.com>
2024-06-01 19:15:19 -0600

    undo database test name

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1:27017/hostedindb2")
+mongoose.connect("mongodb://127.0.0.1:27017/hostedindb")
     .then(db => console.log('Db is connected'))
     .catch(error => console.log(error))
