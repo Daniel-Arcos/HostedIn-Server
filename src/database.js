a144653fca02407f840f8250678788700014a5d3
Daniel Arcos <91172879+Daniel-Arcos@users.noreply.github.com>
2024-06-02 16:28:36 -0600

    Database file

--- src/database.js
+++ src/database.js
@@ -1,5 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect("mongodb://127.0.0.1:27017/hostedindb")
+const user = process.env.USER_BD;
+const password = encodeURIComponent(process.env.PASSWORD_BD);
+const dbName = process.env.NAME_BD; 
+
+mongoose.connect(`mongodb://${user}:${password}@192.168.56.107:27017/${dbName}`)
     .then(db => console.log('Db is connected'))
-    .catch(error => console.log(error))
+    .catch(error => console.error('Error connecting to database:', error));
