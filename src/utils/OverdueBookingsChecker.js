16ed17b9efd9382dc4fdfe1cd80c9d6f8506c3c9
TristanSantiago1 <tristan.santiago520@gmail.com>
2024-06-02 22:08:43 -0600

    OverdueChecker update a cada 5 minutos

--- src/utils/OverdueBookingsChecker.js
+++ src/utils/OverdueBookingsChecker.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron')
 const bookingController = require('../controllers/booking.controller')
 
-cron.schedule('0 0 */12 * * *',() =>{
+cron.schedule('*/5 * * * *',() =>{
     bookingController.checkOverdueBookigns()
     console.log("Ejecutando task cron")
 })
