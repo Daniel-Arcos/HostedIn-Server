73d611a6862e1719a012c89e36d31b8d4e9a79ef
Eduardo García Díaz <eduardo.gd17@gmail.com>
2024-06-11 01:13:35 +0000

    Addecuations for deploy

--- src/database.js
+++ src/database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
+dotenv.config();
 const user = process.env.USER_BD;
 const password = encodeURIComponent(process.env.PASSWORD_BD);
 const dbName = process.env.NAME_BD; 
 
-mongoose.connect(`mongodb://${user}:${password}@192.168.56.107:27017/${dbName}`)
+mongoose.connect(`mongodb://${user}:${password}@192.168.100.26:27017/${dbName}`)
     .then(db => console.log('Db is connected'))
     .catch(error => console.error('Error connecting to database:', error));
